d71460afe17c61a287322a7ba40a1da04615a305
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2024-05-14 11:35:29 +0600

    Blog Updated

--- src/pages/Blogslist.js
+++ src/pages/Blogslist.js
@@ -1,73 +1,74 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import Loader from '../components/Loader'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Loader from "../components/Loader";
+import { Link } from "react-router-dom";
 const Blogslist = () => {
-  const [loader, setLoader] = useState(true)
-  const [allBlogs, setAllBlogs] = useState([])
+  const [loader, setLoader] = useState(true);
+  const [allBlogs, setAllBlogs] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.bumrungraddiscover.com/api/get/blogs')
+    fetch("https://api.bumrungraddiscover.com/api/get/blogs")
       .then((res) => res.json())
       .then((data) => {
-        setAllBlogs(data.data)
-        setLoader(false)
-      })
-  }, [loader])
+        setAllBlogs(data.data);
+        setLoader(false);
+      });
+  }, [loader]);
   const handaleDeleteBlogs = (oneBlogs) => {
-    const aggre = window.confirm(`You Want to Delete, ${oneBlogs?.blogTitle}.`)
+    const aggre = window.confirm(`You Want to Delete, ${oneBlogs?.blogTitle}.`);
     if (aggre) {
       fetch(
         `https://api.bumrungraddiscover.com/api/delete/blogs/${oneBlogs.id}`
       )
         .then((res) => res.json())
         .then((data) => {
-          console.log(data)
+          console.log(data);
           if (data.status === 200) {
             const newBlogData = allBlogs.filter(
               (blogs) => blogs.id !== oneBlogs.id
-            )
-            alert('Blog Deleted Successfully')
-            setAllBlogs(newBlogData)
+            );
+            alert("Blog Deleted Successfully");
+            setAllBlogs(newBlogData);
           }
-        })
+        });
     }
-  }
+  };
 
   return (
-    <div className='mx-5 md:container md:mx-auto py-10 px-5'>
-      <div className='mt-10'>
-        <p className='text-2xl font-semibold'>Blogs List</p>
-        <hr className='my-5' />
+    <div className="mx-5 md:container md:mx-auto py-10 px-5">
+      <div className="mt-10">
+        <p className="text-2xl font-semibold">Blogs List</p>
+        <hr className="my-5" />
         {loader ? (
           <Loader />
         ) : (
-          <div className='grid gap-5 md:grid-cols-2 lg:grid-cols-3 my-10'>
+          <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 my-10">
             {allBlogs?.map((d, i) => (
               <div
                 key={i}
-                className='shadow rounded hover:shadow-xl duration-300 ease-linear flex flex-col justify-between'
+                className="shadow rounded hover:shadow-xl duration-300 ease-linear flex flex-col justify-between"
               >
-                <img src={d.blogImage} alt='' className='' />
-                <div className='p-4'>
-                  {' '}
-                  <h5 className='font-semibold text-blue text-lg'>
+                <img src={d.blogImage} alt="" className="" />
+                <div className="p-4">
+                  {" "}
+                  <h5 className="font-semibold text-blue text-lg">
                     {d.blogTitle}
                   </h5>
                   {/* <p className='my-3 text-justify'>
                     {d.blogSlogan?.slice(0, 160)}...{' '}
                   </p> */}
                 </div>
-                <div className='p-4 flex justify-between'>
-                  {' '}
-                  <Link to={`/home/one-blogs/${d?.slug}`}>
-                    <button className='border border-blue bg-blue hover:bg-white px-2 py-1 rounded hover:text-blue text-white duration-300 ease-linear'>
-                      Update
-                    </button>
+                <div className="flex justify-between">
+                  {" "}
+                  <Link
+                    to={`/home/one-blogs/${d?.slug}`}
+                    className="text-center bg-blue text-white p-1.5 w-full"
+                  >
+                    Update
                   </Link>
                   <button
                     onClick={() => handaleDeleteBlogs(d)}
-                    className='border border-red-400 bg-red-400 hover:bg-white px-2 py-1 rounded hover:text-blue text-white duration-300 ease-linear'
+                    className="text-center bg-red-500 text-white p-1.5 w-full"
                   >
                     Delete
                   </button>
@@ -78,7 +79,7 @@ const Blogslist = () => {
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blogslist
+export default Blogslist;
--- src/pages/OneBlogs.js
+++ src/pages/OneBlogs.js
@@ -1,163 +1,165 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import Loader from '../components/Loader'
-import { Button, Input, Textarea } from '@material-tailwind/react'
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loader from "../components/Loader";
+import { Button, Input, Textarea } from "@material-tailwind/react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useNavigate } from "react-router-dom";
 
 const OneBlogs = () => {
-  const { slug } = useParams()
-  const [loader, setLoader] = useState(true)
-  const [oneBlog, setOneBlog] = useState({})
+  const { slug } = useParams();
+  const [loader, setLoader] = useState(true);
+  const [oneBlog, setOneBlog] = useState({});
   // console.log(oneBlog);
-  const [preview, setPreview] = useState(false)
-  const [loader1, setLoader1] = useState(false)
-  const [blogImg, setBlogImg] = useState('')
+  const [preview, setPreview] = useState(false);
+  const [loader1, setLoader1] = useState(false);
+  const [blogImg, setBlogImg] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   //react quil....
 
-  const [editorValue, seteditorValue] = useState()
+  const [editorValue, seteditorValue] = useState();
 
   const modules = {
     toolbar: [
       [{ header: [1, 2, 3, 4, 5, 6] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      ["bold", "italic", "underline", "strike", "blockquote"],
       [
-        { list: 'ordered' },
-        { list: 'bullet' },
-        { indent: '-1' },
-        { indent: '+1' },
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
       ],
-      ['link', 'image', 'video', 'code-block'],
-      ['clean'],
+      ["link", "image", "video", "code-block"],
+      ["clean"],
     ],
-  }
+  };
 
   const formats = [
-    'header',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    'image',
-  ]
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
   // get data
   useEffect(() => {
     fetch(`https://api.bumrungraddiscover.com/api/get/blogs/${slug}`)
       .then((res) => res.json())
       .then((data) => {
-        setOneBlog(data.data)
-        seteditorValue(data.data.blogDescription)
-        setLoader(false)
-      })
-  }, [slug])
+        setOneBlog(data.data);
+        seteditorValue(data.data.blogDescription);
+        setLoader(false);
+      });
+  }, [slug]);
 
   //update blog
   const handleUpdateBlogs = (e) => {
-    setLoader1(true)
-    e.preventDefault()
-    const name = e.target.name.value
+    setLoader1(true);
+    e.preventDefault();
+    const name = e.target.name.value;
     // const blogslogan = e.target.descriptiion.value
     const blogs = {
       blogImg,
       name,
       // blogslogan,
       editorValue,
-    }
-    console.log(blogs)
+    };
+    console.log(blogs);
 
-    const formData = new FormData()
-    formData.append('blogImage', blogImg)
-    formData.append('blogTitle', name)
+    const formData = new FormData();
+    formData.append("blogImage", blogImg);
+    formData.append("blogTitle", name);
     // formData.append('blogSlogan', blogslogan)
-    formData.append('blogDescription', editorValue)
+    formData.append("blogDescription", editorValue);
     //append data with keys
     fetch(
       `https://api.bumrungraddiscover.com/api/update/blogs/${oneBlog?.id}`,
       {
-        method: 'POST',
+        method: "POST",
         body: formData,
       }
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
-        setLoader1(false)
-        navigate('/home/blogs-list')
-        e.target.reset()
+        console.log(data);
+        setLoader1(false);
+        navigate("/home/blogs-list");
+        e.target.reset();
       })
-      .catch((e) => console.error(e))
-  }
+      .catch((e) => console.error(e));
+  };
   return (
-    <div className='p-5 my-5 md:container md:mx-auto'>
+    <div className="p-5 my-5 md:container md:mx-auto">
       {loader ? (
         <Loader />
       ) : (
         <>
-          <div className='flex justify-between'>
-            <p className='text-2xl font-semibold'>Update Blogs</p>
+          <div className="flex justify-between">
+            <p className="text-2xl font-semibold">Update Blogs</p>
             {preview ? (
               <Button
                 onClick={() => setPreview(!preview)}
-                className='bg-red-500'
+                className="bg-red-500"
               >
                 Cancel
               </Button>
             ) : (
-              <Button onClick={() => setPreview(!preview)} className='bg-blue'>
+              <Button onClick={() => setPreview(!preview)} className="bg-blue">
                 Preview
               </Button>
             )}
           </div>
 
-          <hr className='my-5' />
+          <hr className="my-5" />
           {preview ? (
-            <div className='lg:flex gap-5 bg-white shadow-xl rounded-xl p-5'>
-              <img className='lg:w-1/2' src={oneBlog?.blogImage} alt='' />
-              <div className='lg:w-1/2'>
-                <p>{oneBlog?.blogTitle}</p>
+            <div className="bg-white shadow-xl rounded-xl p-5 flex flex-col gap-2.5 md:gap-5">
+              <div className="flex justify-center">
+                <img className="w-fit h-fit" src={oneBlog?.blogImage} alt="" />
+              </div>
+              <div className="">
+                <h5 className="text-xl font-semibold">{oneBlog?.blogTitle}</h5>
                 {/* <p>{oneBlog?.blogSlogan}</p> */}
               </div>
             </div>
           ) : (
             <form
               onSubmit={handleUpdateBlogs}
-              className='bg-white shadow-xl rounded-xl p-5'
+              className="bg-white shadow-xl rounded-xl p-5"
             >
-              <img className='h-[200px]' src={oneBlog?.blogImage} alt='' />
-              <div className='flex flex-row items-center mt-5'>
+              <img className="h-[200px]" src={oneBlog?.blogImage} alt="" />
+              <div className="flex flex-row items-center mt-5">
                 <input
-                  type='file'
-                  id='custom-input'
+                  type="file"
+                  id="custom-input"
                   onChange={(e) => setBlogImg(e.target.files[0])}
                   hidden
                 />
                 <label
-                  htmlFor='custom-input'
-                  className='block text-sm text-slate-500 mr-4 py-2 px-4 rounded-md border-0 font-semibold bg-blue duration-300 ease-linear text-white cursor-pointer'
+                  htmlFor="custom-input"
+                  className="block text-sm text-slate-500 mr-4 py-2 px-4 rounded-md border-0 font-semibold bg-blue duration-300 ease-linear text-white cursor-pointer"
                 >
                   Choose Blog Image
                 </label>
-                <label className='text-sm text-slate-500'>
-                  {blogImg.name ? blogImg.name : 'No File Chosen'}
+                <label className="text-sm text-slate-500">
+                  {blogImg.name ? blogImg.name : "No File Chosen"}
                 </label>
               </div>
-              <p className='text-red-400 text-sm mt-2.5'>
+              <p className="text-red-400 text-sm mt-2.5">
                 Image Ratio - 1200*628. Image size not more than 500kb
               </p>
-              <div className='my-4 flex flex-col gap-y-4'>
+              <div className="my-4 flex flex-col gap-y-4">
                 <Input
                   defaultValue={oneBlog?.blogTitle}
                   required
-                  label='Blog Title'
-                  name='name'
+                  label="Blog Title"
+                  name="name"
                 />
                 {/* <Textarea
                   defaultValue={oneBlog?.blogSlogan}
@@ -167,23 +169,23 @@ const OneBlogs = () => {
                   rows={8}
                 /> */}
                 <ReactQuill
-                  theme='snow'
+                  theme="snow"
                   modules={modules}
                   formats={formats}
                   value={editorValue}
                   onChange={seteditorValue}
-                  className='my-2.5'
+                  className="my-2.5"
                 />
               </div>
-              <Button className='bg-blue' type='submit'>
-                {loader1 ? 'Loading...' : 'Update Blogs'}
+              <Button className="bg-blue" type="submit">
+                {loader1 ? "Loading..." : "Update Blogs"}
               </Button>
             </form>
           )}
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default OneBlogs
+export default OneBlogs;
